f43b0f13d08ad193b99f541c9a6b95d63534a461
raj-ashwin <ashwinrajmysuru@gmail.com>
2023-12-02 10:07:26 +0530

    Added level 2 security

--- app.js
+++ app.js
@@ -2,11 +2,23 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 const ejs =require("ejs");
-
+const mongoose=require("mongoose")
 const app=express();
+const encrypt=require("mongoose-encryption");
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
+
+mongoose.connect("mongodb://localhost:27017/userDB")
+
+const userSchema = new mongoose.Schema({
+    email:String,
+    password:String
+});
+const secret ="thisisoursecret";
+userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
+
+const User= new mongoose.model("User",userSchema);
 
 app.use(bodyParser.urlencoded({
     extended:true
@@ -24,8 +36,41 @@ app.get("/register",function(req,res){
     res.render("register")
 })
 
+app.post("/register",async function(req,res){
+    const newUser=new User({
+        email:req.body.username,
+        password:req.body.password
+    })
 
+    try{
+        await newUser.save();
+        res.render("secrets");
 
+    }
+    catch(err){
+        res.send("Save unsuccesfull");
+    }
+
+})
+
+app.post("/login",async function(req,res){
+    
+    try{
+    const username=req.body.username;
+    const password=req.body.password;
+    const foundUser= await User.findOne({email:username});
+    if(foundUser.password===password)
+    {
+        res.render("secrets");
+    }
+    else{
+        res.send("INCORRECT CREDENTIALS");
+    }
+    }catch(err)
+    {
+        console.log(err);
+    }
+});
 
 app.listen(3000,function(){
     console.log("Server started at port 3000")
